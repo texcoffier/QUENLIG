4b7666769b7c4b4a46e64d7e4fd6bc4604076b35
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2012-03-06 13:01:57 +0100

    Fix french CSS typo (show stopper)

--- Plugins/change_acl/fr.py
+++ Plugins/change_acl/fr.py
@@ -3,7 +3,7 @@
 import en
 
 css_attributes = (
-    "/A.change_acl:before { content:'Éditer les droits d'accès (ACL)' }",
+    "/A.change_acl:before { content:'Éditer les droits d\\'accès (ACL)' }",
     "/BUTTON.change_acl_save:before { content:'Sauvegarder les droits' }",
     
 ) + en.common
