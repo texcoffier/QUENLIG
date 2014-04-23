eebd1814163ea8f213c41558edf1e04d638391ce
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2014-04-23 12:54:15 +0200

    Real time remaining time decrementation. In red the last minute

--- Plugins/session_duration/session_duration.py
+++ Plugins/session_duration/session_duration.py
@@ -27,9 +27,65 @@ priority_display = 'session_stop'
 font_size = "70%"
 color = "#999"
 acls = { 'Student': ('executable',) }
+javascript = """
+var remaining_seconds, starting_time ;
+function decrement_time(s)
+{
+   remaining_seconds = s ;
+   var now = new Date() ;
+   starting_time = now.getTime() ;
+   setInterval(update_time, 1000) ;
+}
+function duration(t)
+{
+    var s ;
+    var r = t % 60 ;
+    var t = Math.floor(t / 60) ;
+    if (r)
+      {
+        s = r ;
+        if (t == 0)
+            s += 's' ;
+      }
+    else
+      s = "" ;
+    r = t % 60 ;
+    t = Math.floor(t / 60) ;
+    if (t+r && (r != 0 || s != ""))
+        s = r + "m" + s ;
+    r = t % 24 ;
+    t = Math.floor(t / 24) ;
+    if (t+r && (r != 0 || s != ""))
+        s = r + "h" + s ;
+    r = t % 7 ;
+    t = Math.floor(t / 7) ;
+    if(t+r && (r != 0 || s != ""))
+        s = r + "j" + s ;
+    if (t)
+        s = t + "s" + s ;
+    return s ;
+}
+function update_time()
+{
+   var now = new Date() ;
+   var s = remaining_seconds - (now.getTime() - starting_time)/1000 ;
+   s = s.toFixed(0) ;
+   document.getElementById('remaining').innerHTML = duration(s) ;
+   if ( s < 300 )
+     {
+     document.getElementById('remaining').style.color = '#000' ;
+     }
+   if ( s < 60 )
+     {
+     document.getElementById('remaining').style.background = '#F00' ;
+     document.getElementById('remaining').style.color = '#FFF' ;
+     }
+}
+"""
 
 def execute(state, dummy_plugin, dummy_argument):
     if (state.start > state.start_date
         and state.start < state.stop_date):
         t = int(state.stop_date - state.start)
-        return utilities.duration(t)
+        return ('<script>decrement_time(%d)</script><var id="remaining">' % t
+                + utilities.duration(t) + '</var>')
